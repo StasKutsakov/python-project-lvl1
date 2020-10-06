8f3665a3c6d166badb623356e6bed72b94e9f88b
Stas Kutsakov <skutsakov@gmail.com>
2020-10-07 00:20:38 +0300

    step1

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def greeting(string):
+    print("{}".format(string))
+
+
+def main():
+    greeting("Welcome to the Brain Games!")
+
+
+if __name__ == "__main__":
+    main()
