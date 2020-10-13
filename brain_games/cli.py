2e88801c24e8476823dfc757993eb51638bd5d65
Stas Kutsakov <skutsakov@gmail.com>
2020-10-13 23:30:35 +0300

    step3.1

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,7 @@
-def welcome_user(quastion):
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
+import prompt
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
 
-welcome_user('May I have your name?')
+welcome_user()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
+
 def greeting(string):
     print("{}".format(string))
 
 
 def main():
     greeting("Welcome to the Brain Games!")
-    
+    from brain_games.cli import welcome_user
 
 if __name__ == "__main__":
     main()
-
-from brain_games.cli import welcome_user
