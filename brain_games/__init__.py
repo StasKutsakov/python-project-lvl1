26147579946881875e3f99eaf380dd9453f9f326
Stas Kutsakov <skutsakov@gmail.com>
2020-10-16 00:24:36 +0300

    step4

---
+++ brain_games/__init__.py
@@ -0,0 +1,1 @@
+"""The module supplies a function, welcom_user."""
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,10 @@
+
+"""The module supplies a function, welcom_user."""
+
 import prompt
 
-def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
 
-welcome_user()
+def welcome_user():
+    """Get a username and say hello."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {a}!'.format(a=name))
