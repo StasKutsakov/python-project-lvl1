82248e04e049d155c3733e0b4bd164503d220b04
StasKutsakov <skutsakov@gmail.com>
2020-11-23 10:08:29 +0300

    step9.8.2

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -11,7 +11,7 @@ def game():
     multiplier_second = randint(1, 5)
     number_first = x * multiplier_first * min_comm_divisor[0]
     number_second = y * multiplier_second * min_comm_divisor[0]
-    question = str(number_first) + '  ' + str(number_second)
+    question = str(number_first) + '..' + str(number_second)
     if number_first >= number_second:
         t = number_second
         right_answer = t
